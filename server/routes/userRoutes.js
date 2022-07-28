2b7fa0a0553c0a24ba41af47169c223fc86723b2
arisa-e <emmanuelarisa@outlook.com>
2022-07-28 22:26:54 +0300

    initial commit

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+
+const { signup, login }= require("../controller/controller")
+
+router.post('signup', signup)
+router.post('login', login)
+
+module.exports = router
